3ccc10e07576edc71fb33beb7677965b6d5547dd
hyzen0 <hail4heisenberg@gmail.com>
2021-02-07 02:52:41 +0530

    Spinner added.

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import spinner from "../layout/spinner.gif";
 
 const Sprite = styled.img`
   width: 5em;
@@ -10,7 +11,7 @@ const Sprite = styled.img`
 export default class PokemonCard extends Component {
   state = {
     name: "",
-    imageurl: "",
+    imageUrl: "",
     pokemonIndex: "",
   };
   componentDidMount() {
@@ -28,10 +29,26 @@ export default class PokemonCard extends Component {
       <div className="col-md-3 col-sm-6 mb-5">
         <div className="card">
           <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          {this.state.imageLoading ? (
+            <img
+              src={spinner}
+              style={{ width: "5em", height: "5em" }}
+              className="card-img-top rounded mx-auto d-block mt-2"
+            />
+          ) : null}
           <Sprite
-            className="card-img-top rounded mx auto nt-2"
+            className="card-img-top rounded mx-auto mt-2"
             src={this.state.imageUrl}
-          ></Sprite>
+            onLoad={() => this.setState({ imageLoading: false })}
+            onError={() => this.setState({ toManyRequests: true })}
+            style={
+              this.state.toManyRequests
+                ? { display: "none" }
+                : this.state.imageLoading
+                ? null
+                : { display: "block" }
+            }
+          />
           <div className="card-body mx-auto">
             <h6 className="card-title">
               {this.state.name
