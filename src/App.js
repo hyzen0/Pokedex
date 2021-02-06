fbfa65316e54566b7e986ba3367aa61b4622e2bb
hyzen0 <hail4heisenberg@gmail.com>
2021-02-06 20:25:20 +0530

    Data fetched: Use Effect Working.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { getAllPokemon } from "./services/pokemon";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [nextUrl, setNextUrl] = useState("");
-  const [preUrl, setPreUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
   const [loading, setLoading] = useState(true);
-  const intialUrl = "https://pokeapi.co/api/v2/pokemon";
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon";
 
-  useEffect(() =>{
-    async fetchData() {
-      let response = await getAllPokemon( initialUrl)
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      console.log(response);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      setLoading(false);
     }
-  }, [])
-  return <div></div>;
+    fetchData();
+  }, []);
+  return <div>{loading ? <h1>Loading...</h1> : <h1>Data is fetched</h1>}</div>;
 }
 
 export default App;
