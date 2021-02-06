e8fbc018ebe676bf73874164a759e5b12a767071
hyzen0 <hail4heisenberg@gmail.com>
2021-02-07 03:02:35 +0530

    Main Card Changes.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/layout/NavBar";
 import "./App.css";
+import backgroundImage from "./pattern.png";
 import Dashboard from "./components/layout/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ background: `url(${backgroundImage})` }}>
         <NavBar />
         <div className="container">
           <Dashboard />
