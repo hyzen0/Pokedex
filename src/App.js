15835e27c28426c730fe7805def76ecd37c5bb2d
hyzen0 <hail4heisenberg@gmail.com>
2021-02-07 03:13:17 +0530

    Router dom init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/layout/NavBar";
 import "./App.css";
 import backgroundImage from "./pattern.png";
 import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ background: `url(${backgroundImage})` }}>
-        <NavBar />
-        <div className="container">
-          <Dashboard />
+      <Router>
+        <div className="App" style={{ background: `url(${backgroundImage})` }}>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+            <Dashboard />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pokemon/Pokemon.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class pokemon extends Component {
+  render() {
+    return <div></div>;
+  }
+}
