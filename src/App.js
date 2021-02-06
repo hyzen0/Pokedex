05af65b7257f95ce66bef191d810dc43bc807395
hyzen0 <hail4heisenberg@gmail.com>
2021-02-06 21:34:52 +0530

    Buttons

--- src/App.js
+++ src/App.js
@@ -24,6 +24,26 @@ function App() {
     }
     fetchData();
   }, []);
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
   const loadingPokemon = async data => {
     let _pokemonData = await Promise.all(
       data.map(async pokemon => {
@@ -43,10 +63,18 @@ function App() {
       ) : (
         <>
           <NavBar />
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
           <div className="grid-container">
             {pokemonData.map((pokemon, i) => {
               return <Card key={i} pokemon={pokemon} />;
             })}
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
           </div>
         </>
       )}
